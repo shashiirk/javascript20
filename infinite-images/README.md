1bd6ca28c01d6281e34246f336aa05c85e4ae2b9
Shashikanth Reddy <kolanshashii@gmail.com>
2020-12-04 19:52:07 +0530

    finished infinite-images

---
+++ infinite-images/README.md
@@ -0,0 +1,10 @@
+# Infinite Images
+
+Fetch and display images with infinite scroll functionality.
+
+## Project Specifications
+
+- Fetch images from [picsum](https://picsum.photos) API
+- Display certain no.of images per page
+- Custom CSS loader is displayed when scrolled to bottom
+- Next page of images are shown when loading completes
---
+++ infinite-images/assets/scripts/script.js
@@ -0,0 +1,78 @@
+const images = document.getElementById('images');
+const loader = document.getElementById('loader');
+
+// No.of images per page
+const limitValue = 5;
+
+// Current Page Number
+let pageValue = 0;
+
+// Scrolled to bottom before or not
+let scrolledToBottomBefore = false;
+
+// Fetch images from picsum API
+async function getImages() {
+  try {
+    const response = await fetch(
+      `https://picsum.photos/v2/list?limit=${limitValue}&page=${++pageValue}`
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Add images to DOM and display them
+async function displayImages() {
+  try {
+    const imagesData = await getImages();
+
+    // Create an image element for each image in imagesData
+    imagesData.forEach((image) => {
+      // Wrap 'img' with 'a' tag to redirect to a target url when the image is clicked
+      const imageEl = document.createElement('a');
+      imageEl.classList.add('image-link');
+      imageEl.setAttribute('href', image.url);
+      imageEl.setAttribute('target', '_blank');
+      imageEl.innerHTML = `
+        <img src=${image.download_url} alt='image' class='image'>
+      `;
+      // Append to images
+      images.append(imageEl);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Display css loader animation
+function displayLoader() {
+  loader.classList.add('visible');
+
+  // Remove loader after certain amount of time and display next page images
+  setTimeout(() => {
+    loader.classList.remove('visible');
+    displayImages();
+    scrolledToBottomBefore = false;
+  }, 2000);
+}
+
+// Display loader and fetch next page of images when scrolled to bottom
+function scrollHandler() {
+  // Check whether user scrolled to bottom of the window
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1 &&
+    !scrolledToBottomBefore
+  ) {
+    scrolledToBottomBefore = true;
+    displayLoader();
+  }
+}
+
+// Load images initially
+displayImages();
+
+// Listen to scroll event
+window.addEventListener('scroll', scrollHandler);
